59fe45800d250a38363ceab585727a0d1032d090
Nhu Nguyen <nhu313@gmail.com>
2013-12-22 15:57:45 -0600

    brand new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SonnysShop::Application.config.secret_key_base = '513990cdf56ae2515c006cf8bc1fd9c44681070c0643127b40beb75a62c177c943ee59c6102e1a3f826debee098c10fccd6121dbfaf2b2a85f145d67fa1d93b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SonnysShop::Application.config.session_store :cookie_store, key: '_sonnys_shop_session'
